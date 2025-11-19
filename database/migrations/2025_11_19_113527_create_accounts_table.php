0f2860f2d1f890509a85552ee56ed41f79ad77a4
unknown <jaypee.zulieta@sccpag.edu.ph>
2025-11-19 19:54:59 +0800

    fix(accounts_table): Add unique constraint
    Add a composite unique constraint between the
    name attribute and the user_id attribute.

--- database/migrations/2025_11_19_113527_create_accounts_table.php
+++ database/migrations/2025_11_19_113527_create_accounts_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->foreignId('user_id')
                 ->constrained()
                 ->onDelete('cascade');
+            $table->unique(['name', 'user_id']);
             $table->timestamps();
         });
     }
